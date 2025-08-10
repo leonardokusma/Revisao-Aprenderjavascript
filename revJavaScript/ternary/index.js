b72e91d636fa86994a92de9f13071e6300fe443d
leonardo kusma <leonardokusma48@gmail.com>
2025-08-10 20:50:22 -0300

    Add Lembrando do operador ternario

---
+++ revJavaScript/ternary/index.js
@@ -0,0 +1,14 @@
+
+// ternary operator condition ? codeTrue : codeFalse
+// it's good to turn your code easier to read and make our code shorter
+
+let age = 21;
+
+age >= 18 ? "you're an adult" : "You're a child";
+
+
+let time = 16;
+let greeting = time < 12 ? "Good Morning!!" : "Good Afternoon"; 
+
+console.log(greeting);
+
